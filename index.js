2fb3f0d1cfe04571ee063a6962b6c491b5cf809e
AdamTheMaker <adam.bojoh@gmail.com>
2023-02-02 14:43:24 -0500

    made first js connections

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //  The JS fil will have the game logic and interactivity 
+//numberInput  buttonGuess  resetButton  hintButton
+
+const guessInput = document.getElementById("numberInput");
+const guessButton = document.getElementById("buttonGuess")
+
+const h1 = document.querySelector("h1");
+
+console.log(h1, guessButton, guessInput)
