d05d5e131c26f8f9a0b11c5d87ef2d2d6999e706
MrWick97 <120515772+MrWick97@users.noreply.github.com>
2023-02-02 14:29:28 -0600

    number and guess set

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 //numberInput  buttonGuess  resetButton  hintButton
 
 const guessInput = document.getElementById("numberInput");
-const guessButton = document.getElementById("buttonGuess")
+const guessButton = document.getElementById("buttonGuess");
+const resetButton = document.getElementById("resetButton");
+const hintButton = document.getElementById("hintButton");
 
 const h1 = document.querySelector("h1");
 
-console.log(h1, guessButton, guessInput)
+let winner = Math.floor(Math.random() * 100) + 1;
+let attempts = 5 
+guessButton.addEventListener("click", function(){
+    let guessNumber = guessInput.value;
+    console.log(guessNumber)
+})
+
+console.log(h1, guessButton, guessInput, resetButton, hintButton, winner)
